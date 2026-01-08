a97d0418ca7d9e3455a1b68f05533386e2b2934f
TienDat2702 <tiendat97312@gmail.com>
2026-01-08 08:46:40 +0700

    cấu hình view engine ejs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import 'dotenv/config';
+import { time } from 'console';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
 app.get('/', (req, res) => {
-    return res.send('Hello World from Express!');
+    return res.render('home');
+});
+app.get('/demo', (req, res) => {
+    return res.send('Hello World from Express demo!');
+});
+app.get('/demo2', (req, res) => {
+    return res.send('Hello World from Express demo2!');
 });
 
 app.listen(PORT, () => {
