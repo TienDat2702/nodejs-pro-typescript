07ec80b1670c45240bc73c71205a1b46f2015674
TienDat2702 <tiendat97312@gmail.com>
2026-01-08 09:56:35 +0700

    cấu hình static file trong public

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,10 @@ const PORT = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-// routes 
+// config routes
 routeWeb(app);
-
+// config static files: image, css, javascript
+app.use(express.static('public'));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
